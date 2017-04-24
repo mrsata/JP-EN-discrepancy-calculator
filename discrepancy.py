c04fb38645dcbd96101760246c3c518107f8810b
mrsata <liuhanrick@gmail.com>
2017-04-24 18:03:03 -0500

    Update coding style

--- discrepancy.py
+++ discrepancy.py
@@ -1,8 +1,10 @@
 import functools
 import multiprocessing
 import os
+
 ENVOWEL = set(['a', 'e', 'i', 'o', 'u', 'ə', 'ɑ', 'æ', 'ʊ', 'ɪ', 'ɔ'])
 JPVOWEL = set(['a', 'e', 'i', 'o', 'u'])
+
 def preprocess(j):
     prev_c = ""
     final_string = ""
@@ -14,6 +16,7 @@ def preprocess(j):
             final_string += c
         prev_c = c
     return (final_string, rules)
+
 def alignment(pre_rule, main_rule):
     main_rule = main_rule[:]
     final_rule = []
@@ -28,12 +31,13 @@ def alignment(pre_rule, main_rule):
     final_rule = main_rule
     final_rule.extend(pre_rule)
     return list(sorted(final_rule, key=lambda x:x[0]))
+
 def generate_lists(j, e, rule):
     counter = 1
     replace_results = ""
     jap_results = ""
     eng_results = ""
-    for i, j_s in enumerate(j):
+    for i, j_char in enumerate(j):
         matched = False
         for r in rule:
             if i == r[0]:
@@ -43,43 +47,41 @@ def generate_lists(j, e, rule):
                     replace_results += "{m}.ø/{i} ".format(m=str(counter), i=r[1][1])
                     if len(rule) == 1:
                         counter+=1
-                        jap_results += j_s + str(counter)
-                        eng_results += j_s + str(counter)
+                        jap_results += j_char + str(counter)
+                        eng_results += j_char + str(counter)
                 elif r[1][0] == "d":
                     if j[i-1] == '͡':
                         eng_results = eng_results[:-2]
                         replace_results += "{m}.{i}/ø ".format(m=str(counter), i=j[i-2:i+1])
                     else:
-                        replace_results += "{m}.{i}/ø ".format(m=str(counter), i=j_s)
-                    jap_results += j_s + str(counter)
-
+                        replace_results += "{m}.{i}/ø ".format(m=str(counter), i=j_char)
+                    jap_results += j_char + str(counter)
                 elif r[1][0] == "c":
                     if j[i-1] == '͡':
                         eng_results = eng_results[:-2]
-                        replace_results += "{m}.{o}/{i} ".format(m=str(counter),o=j[i-2:i+1],i=r[1][1])
+                        replace_results += "{m}.{o}/{i} ".format(m=str(counter), o=j[i-2:i+1], i=r[1][1])
                     else:
-                        replace_results += "{m}.{o}/{i} ".format(m=str(counter),o=j_s,i=r[1][1])
-                    jap_results += j_s + str(counter)
+                        replace_results += "{m}.{o}/{i} ".format(m=str(counter), o=j_char, i=r[1][1])
+                    jap_results += j_char + str(counter)
                     eng_results += r[1][1] + str(counter)
-
                 else:
                     raise RuntimeError("invalid operation")
                 counter += 1
         if not matched:
             if j[i] == '͡' or j[min(i+1, len(j)-1)] == '͡':
-                jap_results += j_s
-                eng_results += j_s
+                jap_results += j_char
+                eng_results += j_char
                 counter -=1
             else:
-                jap_results += j_s + str(counter)
-                eng_results += j_s + str(counter)
+                jap_results += j_char + str(counter)
+                eng_results += j_char + str(counter)
             counter += 1
     return (jap_results, eng_results, replace_results)
+
 def discrepancy(j, e):
-    #return " ".join(reversed(editDist(j, e, len(j), len(e))[1].split(" ")))
     main_rule = editDist(j, e, len(j), len(e))[1]
     return main_rule
-# @functools.lru_cache(maxsize=None)
+
 def editDist(j, e, m, n):
     # If first string is empty, the only option is to
     # insert all characters of second string into first
@@ -94,6 +96,7 @@ def editDist(j, e, m, n):
 
         if j[m-1] == e[n-1] and j[m-2] == e[n-2] and j[m-1] == e[n-1]:
             return editDist(j, e, m-3, n-3)
+
         if j[m-2] == '͡':
             IS, RM, RP = [editDist(j, e, m, n-1)     # Insert
                         , editDist(j, e, m-3, n)     # Remove
@@ -102,16 +105,14 @@ def editDist(j, e, m, n):
             if MIN == IS[0]:
                 s=IS[1][:]
                 s.append((m-1, ("i", e[n-1])))
-                pass#s = "{}.ø/{} ".format(n, e[n-1]) + IS[1]
             elif MIN == RM[0]:
                 s=RM[1][:]
                 s.append((m-1, ("d", j[m-3:m])))
-                pass#s = "{}.{}/ø ".format(m, j[m-3:m]) + RM[1]
             else:
                 s=RP[1][:]
                 s.append((m-1, ("c", e[n-1])))
-                pass #s = "{}.{}/{} ".format(n, j[m-3:m], e[n-1]) + RP[1]
             return (1 + MIN, s)
+
         if e[n-2] == '͡':
             IS, RM, RP = [editDist(j, e, m, n-3)     # Insert
                         , editDist(j, e, m-1, n)     # Remove
@@ -120,15 +121,12 @@ def editDist(j, e, m, n):
             if MIN == IS[0]:
                 s=IS[1][:]
                 s.append((m-1, ("i", e[n-1])))
-                #s = "{}.ø/{} ".format(n, e[n-3:n]) + IS[1]
             elif MIN == RM[0]:
                 s=RM[1][:]
                 s.append((m-1, ("d", j[m-1])))
-                #s = "{}.{}/ø ".format(m, j[m-1]) + RM[1]
             else:
                 s=RP[1][:]
                 s.append((m-1, ("c", e[n-3:n])))
-                #s = "{}.{}/{} ".format(n, j[m-1], e[n-3:n]) + RP[1]
             return (1 + MIN, s)
 
     # If last characters of two strings are same, nothing
@@ -148,46 +146,35 @@ def editDist(j, e, m, n):
     if MIN == IS[0]:
         s=IS[1][:]
         s.append((m-1, ("i", e[n-1])))
-        #s = "{}.ø/{} ".format(n, e[n-1]) + IS[1]
     elif MIN == RM[0]:
         s=RM[1][:]
         s.append((m-1, ("d", j[m-1])))
-        #s = "{}.{}/ø ".format(m, j[m-1]) + RM[1]
     elif ((e[n-1] in ENVOWEL and j[m-1] in JPVOWEL) or (e[n-1] not in ENVOWEL and j[m-1] not in JPVOWEL) or (j[m-1] == 'a' and e[n-1]=='ɹ')) and j[m-1] != 'j' and not (e[n-1] == "ɹ" and j[m-1] != "a"):
         s=RP[1][:]
         s.append((m-1, ("c", e[n-1])))
-        #s = "{}.{}/{} ".format(n, j[m-1], e[n-1]) + RP[1]
     else:
         MIN = min(IS[0], RM[0])
         if MIN == IS[0]:
             s=IS[1][:]
             s.append((m-1, ("i", e[n-1])))
-            #s = "{}.ø/{} ".format(n, e[n-1]) + IS[1]
         else:
             s=RM[1][:]
             s.append((m-1, ("d", j[m-1])))
-
     return (1 + MIN, s)
-def proc(inp):
-    j, e = inp
+
+def proc(pair):
+    j, e = pair
     no_sokuon, pre_rule = preprocess(j)
-    # print("no shokuon", no_sokuon)
-    # print("eng", e)
     main_rule = discrepancy(no_sokuon, e)
-
-    # print(main_rule)
     final_rule = alignment(pre_rule, main_rule)
+    jp, en, rp = generate_lists(j, e, final_rule)
+    return "{}, {}, {}".format(jp, en, rp)
 
-    eng, jap, rap = generate_lists(j, e, final_rule)
-    # print(rap)
-    return "{}, {}, {}".format(eng, jap, rap)
 n = int(input())  # read a line with a single integer
-
-all_in = []
+pairs = []
 for i in range(1, n+1):
     j, e = [s for s in input().split(" ")]
-    all_in.append((j, e))
-    # editDist.cache_clear()
+    pairs.append((j, e))
 with multiprocessing.Pool(8) as p:
-    results = p.map(proc, all_in)
+    results = p.map(proc, pairs)
 print("\n".join(results))
